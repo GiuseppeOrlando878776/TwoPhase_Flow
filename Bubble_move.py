54462c751979d4bfe43ba1cff38c1e3b284d0cd9
Giuseppe Orlando <giuseppe1.orlando@mail.polimi.it>
2020-05-22 15:29:39 +0200

    Trying incremental Chorin-Temam

--- Bubble_move.py
+++ Bubble_move.py
@@ -47,33 +47,6 @@ class BubbleMove:
         self.eps = self.Param["Interface_Thickness"]
         self.alpha = self.Param["Stabilization_Parameter"]
 
-        #Define an auxiliary dictionary to set proper solution procedure for Navier-Stokes
-        """
-        try:
-            self.switcher_NS = {'Standard':self.solve_Standard_NS_system, \
-                                'ICT':self.solve_ICT_NS_systems}
-        except NameError as e:
-            print("Solution procedure for solving Navier-Stokes " + str(e).split("'")[1] + \
-                  " declared but not implemented")
-            exit(1)
-        assert self.NS_sol_method in self.switcher_NS, \
-               "Solution method for Navier-Stokes not available"
-        """
-
-        #Define an auxiliary dictionary to set proper stabilization
-        try:
-            self.switcher_stab = {'IP':self.IP, 'SUPG':self.SUPG}
-        except NameError as e:
-            print("Stabilization method " + str(e).split("'")[1] + " declared but not implemented")
-            exit(1)
-        assert self.stab_method in self.switcher_stab, \
-               "Stabilization method not available"
-
-        #Check correctness of reinitialization method (in order to avoid typos in particular
-        #since, contrarly to stabilization, it is more difficult to think to other choises)
-        assert self.reinit_method in ['Conservative','Non_Conservative'], \
-               "Reinitialization method not available"
-
         #Compute the Atwood number according to the configuration file
         self.At = (self.rho2 - self.rho1)/(self.rho2 + self.rho1)
 
@@ -115,43 +88,30 @@ class BubbleMove:
 
         #Prepare useful variables for stabilization
         self.h = CellDiameter(self.mesh)
-        if(self.stab_method == 'IP'):
-            self.n_mesh = FacetNormal(self.mesh)
-            self.h_avg  = (self.h('+') + self.h('-'))/2.0
+        self.n_mesh = FacetNormal(self.mesh)
+        self.h_avg  = (self.h('+') + self.h('-'))/2.0
 
-        #Parameters for reinitialization steps
-        hmin = self.mesh.hmin()
-        self.eps_reinit = Constant(hmin)
-        self.alpha_reinit = Constant(0.0625*hmin)
-        self.dt_reinit = Constant(np.minimum(0.0001, 0.5*hmin)) #We choose an explicit treatment to keep the linearity
-                                                                #and so a very small step is needed
-
-        #Define function spaces
-        Velem = VectorElement("Lagrange", self.mesh.ufl_cell(), self.deg + 1)
-        Pelem = FiniteElement("Lagrange" if self.deg > 0 else "DG", self.mesh.ufl_cell(), self.deg)
-
-        #Define FE spaces, trial and test function and suitable functions for NS
-        self.W  = FunctionSpace(self.mesh, Velem*Pelem)
+        #Define FE spaces
+        self.V  = VectorFunctionSpace(self.mesh, "CG", self.deg + 1)
+        self.P  = FunctionSpace(self.mesh, "CG" if self.deg > 0 else "DG", self.deg)
         self.Q  = FunctionSpace(self.mesh, "CG", 2)
         #self.Q2 = VectorFunctionSpace(self.mesh, "CG", 1)
 
         #Define trial and test functions
-        (self.u, self.p) = TrialFunctions(self.W)
-        (self.v, self.q) = TestFunctions(self.W)
+        self.u   = TrialFunction(self.V)
+        self.v   = TestFunction(self.V)
+        self.p   = TrialFunction(self.P)
+        self.q   = TestFunction(self.P)
         self.phi = TrialFunction(self.Q)
         self.l   = TestFunction(self.Q)
 
         #Define functions to store solution
-        self.w_old  = Function(self.W)
-        self.w_curr = Function(self.W)
-        (self.u_curr, self.p_curr) = self.w_curr.split()
-        self.phi_old  = Function(self.Q)
+        self.u_curr   = Function(self.V)
+        self.u_old    = Function(self.V)
+        self.p_curr   = Function(self.P)
+        self.p_old    = Function(self.P)
         self.phi_curr = Function(self.Q)
-
-        #Define useful functions for reinitialization
-        self.phi0 = Function(self.Q)
-        self.phi_intermediate = Function(self.Q) #This is fundamental in case on 'non-conservative'
-                                                 #reinitialization and it is also useful for clearness
+        self.phi_old  = Function(self.Q)
 
         #Define function and vector for plotting level-set and computing volume
         self.tmp = Function(self.Q)
@@ -177,9 +137,9 @@ class BubbleMove:
                 "Initial condition of interface goes outside the domain"
 
         #Assign initial condition
+        self.u_old.assign(interpolate(Constant((0.0,0.1)),self.V))
+        self.p_old.assign(interpolate(Constant(0.0),self.P))
         self.phi_old.assign(interpolate(f,self.Q))
-        self.w_old.assign(interpolate(Constant((0.0,0.0,0.0)),self.W))
-        (self.u_old, self.p_old) = self.w_old.split()
         self.rho_old = self.rho(self.phi_old, self.eps)
         self.mu_old  = self.mu(self.phi_old, self.eps)
 
@@ -190,8 +150,8 @@ class BubbleMove:
 
     """Assemble boundary condition"""
     def assembleBC(self):
-        self.bcs = [DirichletBC(self.W.sub(0), Constant((0.0,0.0)), 'near(x[1], 0.0) || near(x[1], 2.0)'),
-                    DirichletBC(self.W.sub(0).sub(0), Constant(0.0), 'near(x[0], 0.0) || near(x[0], 1.0)')]
+        self.bcs = [DirichletBC(self.V, Constant((0.0,0.0)),  'near(x[1], 0.0) || near(x[1], 2.0)'),
+                    DirichletBC(self.V.sub(0), Constant(0.0), 'near(x[0], 0.0) || near(x[0], 1.0)')]
 
 
     """Auxiliary function to compute density"""
@@ -211,21 +171,12 @@ class BubbleMove:
         return r
 
 
-    """SUPG method"""
-    def SUPG(self, phi, l):
-        r = ((phi - self.phi_old)/self.DT + inner(self.u_curr, grad(phi)))* \
-            self.alpha*self.h/ufl.Max(2.0*sqrt(inner(self.u_curr,self.u_curr)), 4.0/(self.Re*self.h))*\
-            inner(self.u_curr,self.u_curr)*inner(self.u_curr, grad(l))*dx
-        return r
-
-
     """Weak formulation for Navier-Stokes"""
-    def NS_weak_form(self):
+    def ICS_weak_form_1(self):
         F1 = inner(self.rho_old*(self.u - self.u_old)/self.DT, self.v)*dx  \
-           + inner(self.rho_old*dot(self.u_old, nabla_grad(self.u)), self.v)*dx \
+           + inner(self.rho_old*(grad(self.u)*self.u_old), self.v)*dx \
            + 2.0*inner(self.mu_old*D(self.u), D(self.v))*dx \
-           - self.p*div(self.v)*dx \
-           + div(self.u)*self.q*dx \
+           - self.p_old*div(self.v)*dx \
            + 0.98*inner(self.rho_old*self.e2, self.v)*dx \
            #+ 1.0/(self.Bo*self.At)*div(self.n)*inner(self.n, self.v)*CDelta(self.phi_old, self.eps)*dx
 
@@ -236,13 +187,31 @@ class BubbleMove:
         self.A1 = Matrix()
         self.b1 = Vector()
 
+    def ICS_weak_form_2(self):
+        #Define variational problem for step 2
+        self.a1_bis = inner(grad(self.p), grad(self.q))*dx
+        self.L1_bis = inner(grad(self.p_old), grad(self.q))*dx - \
+                      (1.0/self.DT)*div(self.rho_old*self.u_curr)*self.q*dx
+
+        self.A1_bis = assemble(self.a1_bis)
+        self.b1_bis = Vector()
+
+    def ICS_weak_form_3(self):
+        #Define variational problem for step 3
+        self.a1_tris = inner(self.u, self.v)*dx
+        self.L1_tris = inner(self.u_curr, self.v)*dx - \
+                       self.DT*inner(grad(self.p_curr - self.p_old), self.v)*dx
+
+        self.A1_tris = assemble(self.a1_tris)
+        self.b1_tris = Vector()
+
 
     """Level-set weak formulation"""
     def LS_weak_form(self):
         F2 = (self.phi - self.phi_old)/self.DT*self.l*dx \
            + inner(self.u_curr, grad(self.phi))*self.l*dx
 
-        F2 += self.switcher_stab[self.stab_method](self.phi, self.l)
+        F2 += self.IP(self.phi, self.l)
 
         #Save corresponding weak form and declare suitable matrix and vector
         self.a2 = lhs(F2)
@@ -252,45 +221,37 @@ class BubbleMove:
         self.b2 = Vector()
 
 
-    """Weak form non conservative reinitialization"""
-    def NCLSM_weak_form(self):
-        self.approx_sign = signp(self.phi_curr, self.eps_reinit)
-
-        self.a3 = self.phi/self.dt_reinit*self.l*dx
-        self.L3 = self.phi0/self.dt_reinit*self.l*dx + \
-                  self.approx_sign*(1.0 - mgrad(self.phi0))*self.l*dx -\
-                  self.alpha_reinit*inner(grad(self.phi0), grad(self.l))*dx
-
-        #Save the matrix that will not change and declare vector
-        self.A3 = assemble(self.a3)
-        self.b3 = Vector()
-
-
     """Set weak formulations"""
     def set_weak_forms(self):
         #Set variational problem for step 1 (Navier-Stokes)
-        self.NS_weak_form()
+        self.ICS_weak_form_1()
+        self.ICS_weak_form_2()
+        self.ICS_weak_form_3()
 
         #Set variational problem for step 2 (Level-set)
         self.LS_weak_form()
 
-        #Set weak form for level-set reinitialization
-        self.NCLSM_weak_form()
-
 
     """Build and solve the system for Navier-Stokes simulation"""
     def solve_Standard_NS_system(self):
-        # Assemble matrices and right-hand sides
+        #Assemble matrices
         assemble(self.a1, tensor = self.A1)
         assemble(self.L1, tensor = self.b1)
 
-        # Apply boundary conditions
+        #Apply boundary conditions
         [bc.apply(self.A1) for bc in self.bcs]
         [bc.apply(self.b1) for bc in self.bcs]
 
-        #Solve the system
-        solve(self.A1, self.w_curr.vector(), self.b1)
-        #(self.u_curr, self.p_curr) = self.w_curr.split()
+        #Solve the first system
+        solve(self.A1, self.u_curr.vector(), self.b1, 'bicgstab', 'hypre_amg')
+
+        #Assemble and solve the second system
+        assemble(self.L1_bis, tensor = self.b1_bis)
+        solve(self.A1_bis, self.p_curr.vector(), self.b1_bis, 'cg', 'sor')
+
+        #Assemble and solve the third system
+        assemble(self.L1_tris, tensor = self.b1_tris)
+        solve(self.A1_tris, self.u_curr.vector(), self.b1_tris, 'cg', 'sor')
 
 
     """Build the system for Level set simulation"""
@@ -303,38 +264,6 @@ class BubbleMove:
         solve(self.A2, self.phi_curr.vector(), self.b2)
 
         #Compute normal vector (in case we avoid reconstrution)
-        #self.grad_phi.assign(project(grad(self.phi_curr), self.Q2))
-        #self.n = self.grad_phi/sqrt(inner(self.grad_phi, self.grad_phi))
-
-
-    """Build the system for Level set reinitialization"""
-    def Levelset_reinit(self):
-        #Assign current solution and current normal vector to the interface
-        #in case of conservative simulation
-        self.phi0.assign(self.phi_curr)
-        self.approx_sign = signp(self.phi_curr, self.eps_reinit)
-
-        E_old = 1e10
-        for n in range(4):
-            #Assemble and solve the system
-            assemble(self.L3, tensor = self.b3)
-            solve(self.A3, self.phi_intermediate.vector(), self.b3, "cg")
-
-            #Compute the error and check no divergence
-            error = (((self.phi_intermediate - self.phi0)/self.dt_reinit)**2)*dx
-            E = sqrt(abs(assemble(error)))
-
-            if(E_old < E):
-                raise RuntimeError("Divergence at the reinitialization level (iteration " + str(n + 1) + ")")
-            elif(E < 1e-3):
-                break
-
-            #Set previous step solution
-            self.phi0.assign(self.phi_intermediate)
-
-        #Assign the reinitialized level-set to the current solution and
-        #update normal vector to the interface (for Navier-Stokes)
-        self.phi_curr.assign(self.phi_intermediate)
         #self.grad_phi.assign(project(grad(self.phi_curr), self.Q2))
         #self.n = self.grad_phi/sqrt(inner(self.grad_phi, self.grad_phi))
 
@@ -358,7 +287,7 @@ class BubbleMove:
             #plt.show()
             self.vtkfile_phi_draw << (self.tmp, self.t)
             self.vtkfile_u << (self.u_curr, self.t)
-            self.rho_interp.assign(project(self.rho_old,self.Q))
+            self.rho_interp.assign(project(self.rho(self.phi_curr),self.Q))
             self.vtkfile_rho << (self.rho_interp, self.t)
 
         #Check volume consistency
@@ -384,37 +313,23 @@ class BubbleMove:
         #Time-stepping loop
         self.t = self.dt
         self.n_iter = 0
-        self.vtkfile_phi_draw = File('/u/archive/laureandi/orlando/Sim37/phi_draw.pvd')
-        self.vtkfile_u = File('/u/archive/laureandi/orlando/Sim37/u.pvd')
-        self.vtkfile_rho = File('/u/archive/laureandi/orlando/Sim37/rho.pvd')
+        self.vtkfile_phi_draw = File('/u/archive/laureandi/orlando/Sim41/phi_draw.pvd')
+        self.vtkfile_u = File('/u/archive/laureandi/orlando/Sim41/u.pvd')
+        self.vtkfile_rho = File('/u/archive/laureandi/orlando/Sim41/rho.pvd')
         while self.t <= self.t_end:
             begin(int(LogLevel.INFO) + 1,"t = " + str(self.t) + " s")
 
             #Solve Navier-Stokes
             begin(int(LogLevel.INFO) + 1,"Solving Navier-Stokes")
             self.solve_Standard_NS_system()
-            #self.switcher_NS[self.NS_sol_method]()
-            #print(self.u_curr.vector().get_local())
             end()
 
             #Solve level-set
             begin(int(LogLevel.INFO) + 1,"Solving Level-set")
             self.solve_Levelset_system()
-            #print(self.phi_curr.vector().get_local())
             end()
 
-            #Apply reinitialization for level-set
-
-            try:
-                begin(int(LogLevel.INFO) + 1,"Solving reinitialization")
-                self.Levelset_reinit()
-                end()
-            except RuntimeError as e:
-                print(e)
-                print("Aborting simulation...")
-                exit(1)
-
-	        #Save and compute volume
+            #Save and compute volume
             begin(int(LogLevel.INFO) + 1,"Plotting and computing volume")
             self.n_iter += 1
             self.plot_and_volume()
@@ -423,7 +338,7 @@ class BubbleMove:
             end()
 
             #Prepare to next step assign previous-step solution
-            self.w_old.assign(self.w_curr)
+            self.u_old.assign(self.u_curr)
             self.phi_old.assign(self.phi_curr)
             self.rho_old = self.rho(self.phi_old,self.eps)
             self.mu_old = self.mu(self.phi_old,self.eps)
