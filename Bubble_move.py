0d08dccf3df6d7ef3659d2e3d335cffa580f98ad
Giuseppe Orlando <giuseppe1.orlando@mail.polimi.it>
2020-09-27 10:48:32 +0200

    Corrected velocity SUPG

--- Bubble_move.py
+++ Bubble_move.py
@@ -48,7 +48,7 @@ class BubbleMove(TwoPhaseFlows):
 
         #Check correctness of data read
         if(self.rho1 < DOLFIN_EPS or self.rho2 < DOLFIN_EPS or self.mu1 < DOLFIN_EPS or self.mu2 < DOLFIN_EPS \
-           or self.dt < DOLFIN_EPS or self.t_end < DOLFIN_EPS or self.g < DOLFIN_EPS or self.sigma < DOLFIN_EPS):
+           or self.dt < DOLFIN_EPS or self.t_end < DOLFIN_EPS or self.g < 0.0 or self.sigma < 0.0):
             raise ValueError("Invalid parameter read in the configuration file (read a non positive value for some parameters)")
         if(self.dt > self.t_end):
             raise ValueError("Time-step greater than final time")
